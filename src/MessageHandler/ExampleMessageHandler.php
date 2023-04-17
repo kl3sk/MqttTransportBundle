630cc2c2e04f2070ee9999b13f67c0a8c3c5cff9
kl3sk <klesk44@gmail.com>
2023-04-17 15:18:05 +0200

    NS remove unused uses

--- src/MessageHandler/ExampleMessageHandler.php
+++ src/MessageHandler/ExampleMessageHandler.php
@@ -5,9 +5,8 @@ namespace Kl3sk\MqttTransportBundle\MessageHandler;
 use Kl3sk\MqttTransportBundle\Message\ExampleMessage;
 use Monolog\Level;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-use Symfony\Component\Mime\Email;
+
 
 #[AsMessageHandler(fromTransport: 'mqtt')]
 final class ExampleMessageHandler {
